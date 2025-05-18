3b93e84b8ab159af8411c40fc59f4c741bbff466
Alma Ozoria <heisylior@hotmail.com>
2025-05-18 02:09:35 -0400

    Se modifico el tecnico viewmodel

---
+++ app/src/main/java/edu/ucne/registrotecnicos/presentacion/tecnicos/TecnicosViewModel.kt
@@ -0,0 +1,50 @@
+package edu.ucne.registrotecnicos.presentacion.tecnicos
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import edu.ucne.registrotecnicos.data.local.entities.TecnicoEntity
+import edu.ucne.registrotecnicos.data.local.repository.TecnicoRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class TecnicoViewModel(
+    private val repository: TecnicoRepository
+) : ViewModel() {
+
+    // Exponemos el listado de técnicos como StateFlow
+    val tecnicoList: StateFlow<List<TecnicoEntity>> = repository.getAll()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
+
+    fun agregarTecnico(nombre: String, sueldo: Double) {
+        val tecnico = TecnicoEntity(
+            nombre = nombre,
+            sueldo = sueldo
+        )
+        saveTecnico(tecnico)
+    }
+
+    // Guardar o actualizar un técnico
+    fun saveTecnico(tecnico: TecnicoEntity) {
+        viewModelScope.launch {
+            repository.save(tecnico)
+        }
+    }
+
+    // Eliminar un técnico
+    fun delete(tecnico: TecnicoEntity) {
+        viewModelScope.launch {
+            repository.delete(tecnico)
+        }
+    }
+
+    fun update(tecnico: TecnicoEntity) {
+        saveTecnico(tecnico)
+    }
+
+    // Buscar técnico por ID en la lista actual
+    fun getTecnicoById(id: Int?): TecnicoEntity? {
+        return tecnicoList.value.find { it.tecnicoId == id }
+    }
+}
