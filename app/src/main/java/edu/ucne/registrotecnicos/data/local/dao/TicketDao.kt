6c27a279167ae3d7774677bf2c0bc3b44fb89595
Alma Ozoria <heisylior@hotmail.com>
2025-05-18 02:09:52 -0400

    Se creo ticketdao

---
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/dao/TicketDao.kt
@@ -0,0 +1,32 @@
+package edu.ucne.registrotecnicos.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Update
+import androidx.room.Upsert
+import edu.ucne.registrotecnicos.data.local.entities.TicketEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TicketDao {
+    @Upsert
+    suspend fun save(ticket: TicketEntity)
+    @Query("SELECT * FROM tickets WHERE tecnicoId = :tecnicoId")
+    fun getTicketsTecnico(tecnicoId: Int): Flow<List<TicketEntity>>
+
+    @Query(
+        """
+            SELECT * FROM Tickets
+            WHERE ticketId = :id
+            LIMIT 1
+        """)
+    suspend fun find(id: Int): TicketEntity?
+    @Update
+    suspend fun update(ticket: TicketEntity)
+    @Delete
+    suspend fun delete(ticket: TicketEntity)
+
+    @Query("SELECT * FROM Tickets")
+    fun getAll(): Flow<List<TicketEntity>>
+}
