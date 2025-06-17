34c865af9c40b850b567264584cbf8f2d6d166db
Alma Ozoria <heisylior@hotmail.com>
2025-06-17 02:59:04 -0400

    Se agrego EnfermedadApi

---
+++ app/src/main/java/edu/ucne/registrotecnicos/presentacion/remote/EnfermedadApi.kt
@@ -0,0 +1,21 @@
+package edu.ucne.registrotecnicos.presentacion.remote
+
+import edu.ucne.registrotecnicos.presentacion.remote.dto.EnfermedadDto
+import retrofit2.http.*
+
+interface EnfermedadApi {
+    @GET("api/Enfermedades")
+    suspend fun getEnfermedades(): List<EnfermedadDto>
+
+    @GET("api/Enfermedades/{id}")
+    suspend fun getEnfermedad(@Path("id") id: Int): EnfermedadDto
+
+    @POST("api/Enfermedades")
+    suspend fun createEnfermedad(@Body laboratorioDto: EnfermedadDto): EnfermedadDto
+
+    @PUT("api/Enfermedades/{id}")
+    suspend fun updateEnfermedad(@Path("id") id: Int, @Body enfermedadDto: EnfermedadDto): EnfermedadDto
+
+    @DELETE("api/Enfermedades/{id}")
+    suspend fun deleteEnfermedad(@Path("id") id: Int)
+}
