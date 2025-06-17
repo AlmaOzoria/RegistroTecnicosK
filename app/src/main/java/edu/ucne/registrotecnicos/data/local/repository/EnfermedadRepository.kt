d2c0c42e6356b5e9482abf46f5d0f5908ff68c56
Alma Ozoria <heisylior@hotmail.com>
2025-06-17 03:01:04 -0400

    Se creo enfermedad repository

---
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/repository/EnfermedadRepository.kt
@@ -0,0 +1,36 @@
+package edu.ucne.registrotecnicos.data.local.repository
+
+
+import androidx.compose.material3.DatePicker
+import edu.ucne.registrotecnicos.presentacion.remote.dto.EnfermedadDto
+import edu.ucne.registrotecnicos.presentacion.remote.Resource
+import edu.ucne.registrotecnicos.presentacion.remote.DataSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class EnfermedadRepository @Inject constructor(
+    private val dataSource: DataSource
+) {
+    fun getEnfermedades(): Flow<Resource<List<EnfermedadDto>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val enfermedades = dataSource.getEnfermedades()
+            emit(Resource.Success(enfermedades))
+        } catch (e: Exception) {
+            emit(Resource.Error("Error: ${e.message}"))
+        }
+    }
+
+//    suspend fun getEnfermedad(id: Int) = DataSource.getEnfermedad(id)
+
+    suspend fun createEnfermedad(enfermedadDto: EnfermedadDto) =
+        dataSource.createEnfermedad(enfermedadDto)
+
+    suspend fun updateEnfermedad(enfermedadDto: EnfermedadDto) =
+        dataSource.updateEnfermedad(enfermedadDto.enfermedadId ?: 0, enfermedadDto)
+
+    suspend fun deleteEnfermedad(id: Int) =
+        dataSource.deleteEnfermedad(id)
+}
+
